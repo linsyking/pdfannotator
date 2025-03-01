3413f89281a51ca794a0f7f92830c3b341808bcb
linsyking <kxiang@umich.edu>
2025-03-01 14:09:43 -0500

    fix: disable new webkit renderer

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::env;
+
 fn main() {
+    env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
     pdfreader_lib::run()
 }
